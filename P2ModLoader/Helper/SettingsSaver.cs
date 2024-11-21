e72ff670577fb248042a9a8a88b314c5c704513f
SurDno <surdno@yandex.ru>
2024-11-21 11:00:55 +0000

    Update SettingsSaver.cs

--- P2ModLoader/Helper/SettingsSaver.cs
+++ P2ModLoader/Helper/SettingsSaver.cs
@@ -7,6 +7,7 @@ public static class SettingsSaver {
     private static readonly string SettingsPath = Path.Combine(SETTINGS_DIRECTORY, "settings.json");
     private static readonly JsonSerializerOptions Options = new JsonSerializerOptions { WriteIndented = true };
     private static bool _subscribed = false;
+    private static bool _pauseSaving;
     
     private class SavedSettings {
         public string? InstallPath { get; init; }
@@ -16,18 +17,25 @@ public static class SettingsSaver {
         public List<SavedModState> ModState { get; init; } = [];
     }
     
+    public static void PauseSaving() => _pauseSaving = true; 
+    public static void UnpauseSaving() => _pauseSaving = false; 
+    
     public static void LoadSettings() {
         if (File.Exists(SettingsPath)) {
             try {
                 var settings = JsonSerializer.Deserialize<SavedSettings>(File.ReadAllText(SettingsPath));
 
-                if (settings == null) return;
+                if (settings == null) {
+                    Logger.LogInfo("No settings.json file has been found, default settings will be used.");
+                    return;
+                }
 
                 SettingsHolder.InstallPath = settings.InstallPath == "null" ? null : settings.InstallPath;
                 SettingsHolder.AllowStartupWithConflicts = settings.AllowStartupWithConflicts;
                 SettingsHolder.IsPatched = settings.IsPatched;
                 SettingsHolder.CheckForUpdatesOnStartup = settings.CheckForUpdates;
                 SettingsHolder.LastKnownModState = settings.ModState;
+                Logger.LogInfo("Applied settings from settings.json.");
             } catch (Exception ex) {
                 ErrorHandler.Handle("Failed to load settings", ex);
             }
@@ -36,12 +44,16 @@ public static class SettingsSaver {
         if (_subscribed) return;
         SettingsHolder.InstallPathChanged += SaveSettings;
         SettingsHolder.StartupWithConflictsChanged += SaveSettings;
+        SettingsHolder.CheckForUpdatesOnStartupChanged += SaveSettings;
         SettingsHolder.PatchStatusChanged += SaveSettings;
         SettingsHolder.ModStateChanged += SaveSettings;
         _subscribed = true;
     }
     
     private static void SaveSettings() {
+        if (_pauseSaving) return;
+        
+        Logger.LogInfo($"Saving new settings to settings.json.");
         try {
             Directory.CreateDirectory(SETTINGS_DIRECTORY);
             
@@ -55,7 +67,7 @@ public static class SettingsSaver {
             
             File.WriteAllText(SettingsPath, JsonSerializer.Serialize(settings, Options));
         } catch (Exception ex) {
-            ErrorHandler.Handle("Failed to load settings", ex);
+            ErrorHandler.Handle("Failed to save settings", ex);
         }
     }
 }
