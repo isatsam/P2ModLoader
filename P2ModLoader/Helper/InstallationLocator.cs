3d58f5354563d97310f01a59ff6550337194d690
isatsam <isatsamdev@gmail.com>
2024-12-06 21:07:22 +0300

    Detect Linux installation path via "Locate" button

--- P2ModLoader/Helper/InstallationLocator.cs
+++ P2ModLoader/Helper/InstallationLocator.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using Microsoft.Win32;
 
@@ -6,11 +7,13 @@ namespace P2ModLoader.Helper;
 public static class InstallationLocator {
     private const string STEAM_32_BIT_PATH = @"SOFTWARE\Valve\Steam";
     private const string STEAM_64_BIT_PATH = @"SOFTWARE\Wow6432Node\Valve\Steam";
+
+    private const string STEAM_LINUX_PATH = @"\.local\share\Steam"; // example: /home/username/.local/share/Steam
     
-    private const string PATHOLOGIC_2_STEAM_APP_ID = "505230";
+    private const string PATHOLOGIC_2_STEAM_APP_ID = "230410";
     
     private const string STEAM_LIBRARY_FOLDERS_PATH = @"config\libraryfolders.vdf";
-    private const string PATHOLOGIC_STEAM_RELATIVE_PATH = @"steamapps\common\Pathologic";
+    private const string PATHOLOGIC_STEAM_RELATIVE_PATH = @"steamapps\common\Warframe";
 
     private const string APPDATA_PATH = @"AppData\LocalLow\Ice-Pick Lodge\Pathologic 2";
     
@@ -20,20 +23,76 @@ public static class InstallationLocator {
 
         return steamPath;
     }
-    
-    public static string? FindInstall() {
+
+    public static string? FindSteamLinux()
+    {
+        if (!Path.Exists(@"Z:\home"))
+        {
+            // Likely not using Wine (i.e. on Windows), or non-standard Wine setup
+            return null;
+        }
+
+        var user = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        user = user[(user.LastIndexOf('\\')+1)..]; // "DEVICENAME/username" -> "username"
+
+        var steamPath = Path.Join(@"Z:\home\", user, STEAM_LINUX_PATH);
+        return steamPath;
+    }
+
+    public static string? FindGog()
+    {
+        // TODO
+        Logger.LogWarning("FindGog -- not implemented");
+        var gogPath = "";
+        return gogPath;
+    }
+
+    public static string? FindInstall()
+    {
         var steamPath = FindSteam();
-        
-        // TODO: Handle GOG installs.
-        return string.IsNullOrEmpty(steamPath) ? null : FindSteamInstall(steamPath);
+        if  (!string.IsNullOrEmpty(steamPath))
+        {
+            Logger.LogInfo("Found Steam installation:   " + steamPath);
+            return FindSteamInstall(steamPath);
+        };
+
+
+        steamPath = FindSteamLinux();
+        if (!string.IsNullOrEmpty(steamPath))
+        {
+            Logger.LogInfo("Found Steam installation:   " + steamPath);
+            return FindSteamInstall(steamPath);
+        }
+
+        var gogPath = FindGog();
+        if (!string.IsNullOrEmpty(gogPath))
+        {
+            Logger.LogInfo("Found GOG installation:   " + gogPath);
+            return FindGogInstall(gogPath);
+        }
+
+        return null;
+        }
+
+    private static string? FindGogInstall(string gogPath)
+    {
+        return null;
     }
 
     private static string? FindSteamInstall(string steamPath) {
         var libraryFoldersPath = Path.Combine(steamPath, STEAM_LIBRARY_FOLDERS_PATH);
-        if (!File.Exists(libraryFoldersPath)) return null;
+        Logger.LogInfo("libraryFoldersPath: " + libraryFoldersPath + " ; " + File.Exists(libraryFoldersPath));
+        if (!File.Exists(libraryFoldersPath))
+        {
+            libraryFoldersPath = Path.Combine(steamPath, STEAM_LIBRARY_FOLDERS_PATH.ToLower());
+            if (!File.Exists(libraryFoldersPath))
+            {
+                return null;
+            }
+        }
 
         var installPath = FindPathologicSteamPath(libraryFoldersPath);
-
+        Logger.LogInfo("installPath: " + installPath);
         return installPath;
     }
 
@@ -57,7 +116,9 @@ public static class InstallationLocator {
 
             var pathMatch = pathRegex.Match(line);
             if (pathMatch.Success)
+            {
                 currentPath = pathMatch.Groups[1].Value.Replace(@"\\", @"\");
+            }
 
             if (currentPath != null && appRegex.IsMatch(line)) {
                 foundApp = true;
@@ -70,7 +131,7 @@ public static class InstallationLocator {
 
         if (!foundApp)
             return null;
-        
+
         return currentPath != null ? Path.Combine(currentPath, PATHOLOGIC_STEAM_RELATIVE_PATH) : null;
     }
 
